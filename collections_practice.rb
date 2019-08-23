fa6edb7f829e1c3c6f028ee23f8f76202d28effb
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 03:09:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
-
+def sort_array_asc(array)
+  array.sort { |a, b| a <=> b } 
+end 
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end 
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length}
+end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+def reverse_array
+end 
+def kesha_maker
+end
+def find_a
+end 
+def sum_array
+end 
+def add_s(array)
+  array.each_with_index.map do |call, index|
+    if index == 1
+      call
+    else 
+    call << "s" 
+end 
+end
+end
