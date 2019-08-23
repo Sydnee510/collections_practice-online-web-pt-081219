6deb5770cda07ef9fd720f392725790e4463925a
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 03:17:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,9 +18,7 @@ def kesha_maker(array)
   
 end
 def find_a()
-  input = "a"
-match = dictionary.select { |a| a.match(input) }
-puts match
+dictionary.select { |word| word.start_with?('a') }
 end 
 def sum_array(array)
   array.sum
